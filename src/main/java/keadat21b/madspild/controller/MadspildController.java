a58f0885a1096dee7a2ab40ef8c39da90fecca56
RedCupCrowe <valdemarkjohansen@gmail.com>
2022-03-02 20:32:26 +0100

    artikelside tilføjet, skal bare lige styles lidt

--- src/main/java/keadat21b/madspild/controller/MadspildController.java
+++ src/main/java/keadat21b/madspild/controller/MadspildController.java
@@ -2,6 +2,7 @@ package keadat21b.madspild.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MadspildController {
@@ -14,6 +15,11 @@ public String index(){
 @GetMapping("/info")
     public String info(){
         return "info";
+}
+
+@GetMapping("/artikel")
+    public String artikel(){
+        return "artikel";
 }
 
 
