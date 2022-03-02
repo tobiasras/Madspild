0105d7d74945816a202e8f446bc46721ab97a493
Baeist <lars18n2@stud.kea.dk>
2022-03-02 11:27:32 +0100

    Opsæt til localhost

---
+++ src/main/java/keadat21b/madspild/controller/MadspildController.java
@@ -0,0 +1,15 @@
+package keadat21b.madspild.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MadspildController {
+
+    @GetMapping("/")
+public String index(){
+    return "index";
+}
+
+
+}
